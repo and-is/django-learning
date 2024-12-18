094efe858e3648af49d929ef539a468a5a147902
Andis Paudel <andispaudel@gmail.com>
2024-12-18 11:37:34 +0545

    worked through serializers and views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,17 @@
-from django.http import JsonResponse, HttpResponse
-import json
+#from django.http import JsonResponse, HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from products.models import Product
 from django.forms.models import model_to_dict
+from products.serializers import ProductSerializer
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
+    """
+    DRF API View
+    """
+    # instance = Product.objects.all().order_by("?").first()
+    
     # if model_data:
     #     data['id'] = model_data.id
     #     data["title"] = model_data.title
@@ -15,10 +21,15 @@ def api_home(request, *args, **kwargs):
     #     # model instance (model_data), turn to python dict
     #     # return JSON
 
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
+    # if instance:
+    #     #data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+    #     data = ProductSerializer(instance).data
+    serializer = ProductSerializer(data = request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
     #     data = dict(data)
     #     json_data_str = json.dumps(data)
     # return HttpResponse(json_data_str, headers={"content-type": "application/json"})
-
+    return Response({"invalid": "not good data"}, status=400)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 # endpoint = "https://httpbin.org/"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"product_id": 123 })   # HTTP Request
+get_response = requests.post(endpoint, json={"title":"abc123", "content": "Hello world", "price":"sad" })   # HTTP Request
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
