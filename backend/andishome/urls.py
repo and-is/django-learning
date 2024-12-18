4b8f99e9dee42e1536eaefbb4a80a9f4931bcf5a
Andis Paudel <andispaudel@gmail.com>
2024-12-18 11:49:37 +0545

    touched generics

--- backend/andishome/urls.py
+++ backend/andishome/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),    # references urls.py inside api app
+    path('api/products/', include('products.urls'))
 ]
